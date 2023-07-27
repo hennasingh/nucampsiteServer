7d03362b125ea8beb918507b9a3d78f6771a329d
Henna Singh <hennasingh04@gmail.com>
2023-07-27 17:48:41 +0100

    implemented multer middleware to enable file uploads to express server

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const authenticate = require('./authenticate');
+const uploadRouter = require('./routes/uploadRouter');
 
 //connecting mongodb server with express
 const mongoose = require('mongoose');
@@ -63,6 +64,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
