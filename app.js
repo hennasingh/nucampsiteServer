ffa9e48442bbc7eda2c970e1cc04a4d20dcac2eb
Henna Singh <hennasingh04@gmail.com>
2023-07-30 15:34:26 +0100

    implemented favorite model and favoriteRouter to handle CRUD requests to the express server

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const authenticate = require('./authenticate');
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 //connecting mongodb server with express
 const mongoose = require('mongoose');
@@ -65,6 +66,7 @@ app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
 
 // catch 404 and forward to error handler
